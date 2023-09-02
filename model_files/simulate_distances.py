0967a2ab2cc9b629d2488ba9c7029db383989a18
Sam M. Ward <smward1@hotmail.com>
2023-09-02 22:38:20 +0100

    Improved commenting

--- model_files/simulate_distances.py
+++ model_files/simulate_distances.py
@@ -6,7 +6,7 @@ Module contains class for simulating individual siblings distance estimates
 Contains:
 --------------------
 SiblingsDistanceSimulator class:
-	inputs: Ngal=30, Sg = 2, sigma0=0.1, sigmaRel=0.05, mu_bounds = [25,35], sigma_fit_s=0.05
+	inputs: Ng=30, Sg = 2, sigma0=0.1, sigmaRel=0.05, mu_bounds = [25,35], sigma_fit_s=0.05
 --------------------
 
 Written by Sam M. Ward: smw92@cam.ac.uk
@@ -18,28 +18,28 @@ import copy
 
 class SiblingsDistanceSimulator:
 
-    def __init__(self, Ngal=30, Sg = 2, sigma0=0.1, sigmaRel=0.05, mu_bounds = [25,35], sigma_fit_s=0.05):
+    def __init__(self, Ng=30, Sg = 2, sigma0=0.1, sigmaRel=0.05, mu_bounds = [25,35], sigma_fit_s=0.05):
         """
         Initialisation
 
         Parameters
         ----------
-        Ngal : int
+        Ng : int (optional; default=30)
             Number of siblings galaxies
 
-        Sg : int or list/array of ints
+        Sg : int or list/array of ints (optional; default=2)
             Number of siblings per galaxy
 
-        sigma0 : float>0
+        sigma0 : float>0 (optional; default=0.1)
             total intrinsic scatter
 
-        sigmaRel : float>0 and <sigma0
+        sigmaRel : float>0 and <sigma0 (optional; default=0.5)
             relative intrinsic scatter
 
-        mu_bounds : list/array of x2 floats, each>0
+        mu_bounds : list/array of x2 floats, each>0 (optional; default=[25,35])
             random distances drawn from [mu_low, mu_high]
 
-        sigma_fit_s : list/array of floats, each>0, or float
+        sigma_fit_s : list/array of floats, each>0, or float (optional; default=0.05)
             the fitting uncertainties, i.e. measurement errors, on each individual siblings distance estimate
 
         End Product(s)
@@ -51,7 +51,7 @@ class SiblingsDistanceSimulator:
         self.sigmaCommon = np.sqrt(np.square(self.sigma0)-np.square(self.sigmaRel))
 
         #Ensure Input Data Types are Correct
-        assert(type(self.Ngal) is int)
+        assert(type(self.Ng) is int)
         assert(type(self.sigma0) in [float,int] and self.sigma0>0)
         assert(type(self.sigmaRel) in [float,int] and self.sigmaRel>=0 and self.sigmaRel<=self.sigma0)
         assert(type(self.mu_bounds) is list and len(self.mu_bounds)==2)
@@ -70,17 +70,17 @@ class SiblingsDistanceSimulator:
             elif type(y) in [int,float]:#If int/floats, create list
                 if x=='Sg':
                     assert(type(y) is int)
-                    self.__dict__[x] = [y for _ in range(self.Ngal)]
+                    self.__dict__[x] = [y for _ in range(self.Ng)]
                 if x=='sigma_fit_s':
                     assert(y>0)
-                    self.__dict__[x] = [y for _ in range(self.Ngal) for __ in range(self.Sg[_])]
+                    self.__dict__[x] = [y for _ in range(self.Ng) for __ in range(self.Sg[_])]
 
         #Empty pandas df
         dfmus = pd.DataFrame(columns=['Galaxy','SN','mus','mu_errs'])
 
         #For each galaxy, simulate individual siblings distances, collate results into pandas df
         counter = -1
-        for g in range(self.Ngal):
+        for g in range(self.Ng):
             dM_Common   = np.random.normal(0,self.sigmaCommon)
             dM_Rels     = np.random.normal(0,self.sigmaRel,self.Sg[g])
             mu          = np.random.uniform(self.mu_bounds[0],self.mu_bounds[1])
