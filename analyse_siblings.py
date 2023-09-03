e0d6a5f5a8f937ca3eb35d8c3ecefa49be8660e5
Sam M. Ward <smward1@hotmail.com>
2023-09-03 14:50:43 +0100

    Multigal analysis

--- analyse_siblings.py
+++ analyse_siblings.py
@@ -6,12 +6,15 @@ from sigmaRel import *
 from simulate_distances import *
 
 
-simulator = SiblingsDistanceSimulator(Ng=30,Sg=2)
+simulator = SiblingsDistanceSimulator(Ng=100,Sg=3,external_distances=True)
 dfmus     = simulator.dfmus
 
-sibs = multi_galaxy_siblings(dfmus)
-#sibs.loop_single_galaxy_analyses()
-sibs.compute_analytic_multi_gal_sigmaRel_posterior()
+multigal = multi_galaxy_siblings(dfmus)
+#multigal.loop_single_galaxy_analyses()
+#multigal.compute_analytic_multi_gal_sigmaRel_posterior()
+
+#multigal.sigmaRel_sampler(sigma0=0.1,sigmapec=250,use_external_distances=True)
+multigal.sigmaRel_sampler(sigma0='free',sigmapec='free',use_external_distances=True)
 
 '''
 for gal in dfmus['Galaxy'].unique():
