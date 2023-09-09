b572ac1d215cd342028e88d8d6a2ce15f683ccca
Sam M. Ward <smward1@hotmail.com>
2023-09-09 16:50:39 +0100

    Latest script

--- analyse_siblings.py
+++ analyse_siblings.py
@@ -9,38 +9,9 @@ from simulate_distances import *
 simulator = SiblingsDistanceSimulator(Ng=100,Sg=3,external_distances=True)
 dfmus     = simulator.dfmus
 
-multigal = multi_galaxy_siblings(dfmus)
+multigal = multi_galaxy_siblings(dfmus,sigma0='free',sigmapec='free',use_external_distances=True)
+#multigal = multi_galaxy_siblings(dfmus,sigma0='free',sigmapec=250,use_external_distances=True)
 #multigal.loop_single_galaxy_analyses()
 #multigal.compute_analytic_multi_gal_sigmaRel_posterior()
-multigal.sigmaRel_sampler(sigma0='free',sigmapec='free',use_external_distances=True)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#NGC 3147
-'''
-DATA = {
-	#'RVfixed':{ 'mus': np.array([33.142,33.135,33.224]),
-	#            'errors':np.array([0.053,0.065,0.070])},
-	'RVfree' :{ 'mus': np.array([33.131,33.127,33.256]),
-				'errors':np.array([0.071,0.128,0.198]),
-				'names':['2021hpr','1997bq','2008fv'],
-                'galname':'NGC3147'}
-				}
-for MODE in DATA:
-	sibgal = siblings_galaxy(DATA[MODE]['mus'],DATA[MODE]['errors'],DATA[MODE]['names'],DATA[MODE]['galname'])
-	sibgal.plot_sigmaRel_posteriors()
-	sibgal.plot_individual_distances()
-	sibgal.combine_individual_distances()
-	sibgal.plot_common_distances()
-'''
+multigal.sigmaRel_sampler()#sigma0='free',sigmapec='free',use_external_distances=True)
+multigal.plot_posterior_samples()
