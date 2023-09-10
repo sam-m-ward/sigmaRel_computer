f7808d92d8d96eb1e04dd609f1b567f9414f03af
Sam M. Ward <smward1@hotmail.com>
2023-09-10 14:50:46 +0100

    Multi prior bounds

--- analyse_siblings.py
+++ analyse_siblings.py
@@ -12,6 +12,6 @@ dfmus     = simulator.dfmus
 multigal = multi_galaxy_siblings(dfmus,sigma0='free',sigmapec='free',use_external_distances=True)
 #multigal = multi_galaxy_siblings(dfmus,sigma0='free',sigmapec=250,use_external_distances=True)
 #multigal.loop_single_galaxy_analyses()
-#multigal.compute_analytic_multi_gal_sigmaRel_posterior()
+#multigal.compute_analytic_multi_gal_sigmaRel_posterior(prior_upper_bounds=[0.1,0.15,1.0])
 multigal.sigmaRel_sampler()#sigma0='free',sigmapec='free',use_external_distances=True)
 multigal.plot_posterior_samples()
