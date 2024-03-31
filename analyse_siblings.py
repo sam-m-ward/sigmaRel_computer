4afd8319fa1ade0f143eb1c1e90a83e28688ef6a
Sam M. Ward <smward1@hotmail.com>
2024-03-31 12:12:20 +0100

    Most recent script

--- analyse_siblings.py
+++ analyse_siblings.py
@@ -3,72 +3,59 @@ import sys
 rootpath = './'
 sys.path.append(rootpath+'model_files/')
 from sigmaRel import *
-#from simulate_distances import *
-#simulator = SiblingsDistanceSimulator(Ng=100,Sg=3,external_distances=True)
-#dfmus     = simulator.dfmus
+
+'''
+from simulate_distances import *
+simulator = SiblingsDistanceSimulator(Ng=200,Sg=2,external_distances=True,sigmaRel=0.0999,zcmberr=1e-5)
+dfmus     = simulator.dfmus
+print (dfmus)
+#multigal = multi_galaxy_siblings(dfmus,sigma0='free',sigmapec='free',use_external_distances=True)
+#multigal = multi_galaxy_siblings(dfmus,sigma0='free',eta_sigmaRel_input=None,sigmapec=250,use_external_distances=True)
+multigal = multi_galaxy_siblings(dfmus,sigma0=1.0,eta_sigmaRel_input=None,sigmapec=250,use_external_distances=False)
+multigal.compute_analytic_multi_gal_sigmaRel_posterior()
+#multigal.sigmaRel_sampler()#sigma0=None, sigmapec=None, eta_sigmaRel_input=None, use_external_distances=None, overwrite=True)
+#multigal.plot_posterior_samples()
+#multigal.sigmaRel_sampler(sigma0='free',sigmapec=250,use_external_distances=True,alt_prior=False)
+#multigal.plot_posterior_samples()
+#multigal.sigmaRel_sampler(sigma0='free',sigmapec=250,use_external_distances=True,alt_prior=True)
+#multigal.plot_posterior_samples()
+multigal.sigmaRel_sampler(sigma0='free',sigmapec='free',use_external_distances=True,zmarg=False,overwrite=True,alt_prior=True)
+multigal.plot_posterior_samples()
+#multigal.sigmaRel_sampler(sigma0='free',sigmapec='free',use_external_distances=True,zmarg=False,overwrite=True,alt_prior=False)
+#multigal.plot_posterior_samples()
+
+err=1/0
+#'''
 
 
-#LOAD IN CHAINS
-from glob import glob
-chains_path = '../../bayesn_for_ztf/bayesn-pre-release-dev/ZTFtest3/ZTFtest3_Samples/'
-#chains_path = '../../bayesn_for_ztf/bayesn-pre-release-dev/ZTFtest3_freeRV/ZTFtest3_freeRV_Samples/'
-files = glob(chains_path+'*.npy')
-snes  = {ff.split(chains_path)[1].split('_ZTF_DR2.snana_chains.npy')[0]:ff for ff in files}
-dfmus = {'sn':[]} ; PARS = ['mu','AV','theta']
-for sn,ff in snes.items():
-    dfmus['sn'].append(sn)
-    for PAR in PARS:
-        if PAR not in dfmus.keys():
-            dfmus[PAR], dfmus[PAR+'_errs'], dfmus[PAR+'_samps'] = [],[],[]
-        x = np.load(ff,allow_pickle=True).item()
-        if PAR=='mu':   dfmus[f'{PAR}_samps'].append(x[PAR]+x['delM'])
-        else:           dfmus[f'{PAR}_samps'].append(x[PAR])
-        dfmus[PAR].append(np.median(dfmus[f'{PAR}_samps'][-1]))
-        dfmus[f'{PAR}_errs'].append(np.std(dfmus[f'{PAR}_samps'][-1]))
-dfmus = pd.DataFrame(dfmus)
-dfmus.set_index('sn',inplace=True)
+from load_data import *
+dfmus = load_dfmus('ZTFtest5')
 
-#LOAD IN SUMMARIES
-files = glob(chains_path+'*.pkl')
-snes  = {ff.split(chains_path)[1].split('_ZTF_DR2.snana_summary.pkl')[0]:ff for ff in files}
-Rhats = {}
-Rhat_cols = [f'{PAR}_Rhats' for PAR in PARS]
-for sn,ff in snes.items():
-    with open(ff,'rb') as f:
-        summary = pickle.load(f)
-    dfmus.loc[sn,Rhat_cols] = summary.loc[PARS]['R_hat'].values
-
-#LOAD IN SNE TO MAP TO GALAXY
-sng = '../../bayesn_for_ztf/bayesn-pre-release-dev/bayesn-data/lcs/meta/ztf_dr2_siblings_redshifts.txt'
-sng = pd.read_csv(sng,names=['sn','z'],sep='\s')
-sn_to_g = {sn:1+isn//2 for isn,sn in enumerate(sng.sn)}
-
-
-#COMBINE CHAINS AND SIBS GALAXIES
-dfmus = dfmus.loc[sn_to_g.keys()]
-dfmus['Galaxy'] = list(sn_to_g.values())
-data = {
-    'Galaxy':list(sn_to_g.values()),
-    'SN':list(sn_to_g.keys()),
-}
-for PAR in ['mu','AV','theta']:
-    data[f'{PAR}s']      = [dfmus[PAR].loc[sn] for sn in sn_to_g]
-    data[f'{PAR}_errs']  = [dfmus[f'{PAR}_errs'].loc[sn] for sn in sn_to_g]
-    data[f'{PAR}_samps'] = [dfmus[f'{PAR}_samps'].loc[sn] for sn in sn_to_g]
-    data[f'{PAR}_Rhats'] = [dfmus[f'{PAR}_Rhats'].loc[sn] for sn in sn_to_g]
-
-dfmus = pd.DataFrame(data=data)
-print (dfmus.columns)
+#Inflate error for 20abmarcv_ siblings
+#dfmus[dfmus['Galaxy']==1]['zcmb_errs'] = 0.01
 
 #APPLY FILTERING
-#dfmus = dfmus[~dfmus['Galaxy'].isin([10,11])]
-#dfmus = dfmus[~dfmus['Galaxy'].isin([3,5,10,11])]
-#dfmus = dfmus[~dfmus['Galaxy'].isin([5,10,11])]
-#dfmus = dfmus[~dfmus['Galaxy'].isin([2,5,10,11])]
+#dfmus = dfmus[~dfmus['Galaxy'].isin([1,3,6,11,12])]
+#dfmus = dfmus[~dfmus['Galaxy'].isin([3,11,12])]
+#dfmus = dfmus[~dfmus['Galaxy'].isin([11,12])]
+#dfmus = dfmus[~dfmus['Galaxy'].isin([3,11,12])]
+dfmus = dfmus[~dfmus['Galaxy'].isin([3,4,11,12])]
+#dfmus = dfmus[~dfmus['Galaxy'].isin([3,6,11,12])]
+#dfmus = dfmus[~dfmus['Galaxy'].isin([3,4,6,11,12])]
+#dfmus = dfmus[~dfmus['Galaxy'].isin([6,11,12])]
 
 #ANALYSE
 multigal = multi_galaxy_siblings(dfmus,sigma0='free',sigmapec=250,use_external_distances=True)
 #multigal.print_table()
-multigal.compute_analytic_multi_gal_sigmaRel_posterior(prior_upper_bounds=[1.0],blind=True)
-multigal.plot_parameters(['mu','AV','theta'])
+multigal.compute_analytic_multi_gal_sigmaRel_posterior(prior_upper_bounds=[1.0],blind=False)
+#multigal.plot_parameters(['mu','AV','theta'])
+'''
+multigal.plot_parameters(['mu','theta','AV'],g_or_z = 'g', subtract_g_mean=None)
+multigal.plot_parameters('mu',g_or_z = 'g')
+multigal.plot_parameters(['mu','theta','AV'],g_or_z = 'z')
+multigal.plot_parameters('mu',g_or_z = 'z')
+#'''
+#multigal.n_warmup   = 2000 ; multigal.n_sampling = 8000
+#multigal.sigmaRel_sampler(sigma0='free',sigmapec=250,use_external_distances=True,zmarg=False,alt_prior=True,overwrite=True)
+#multigal.plot_posterior_samples()
 #'''
