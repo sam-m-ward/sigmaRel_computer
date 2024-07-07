4b0660d7ae1e3ea6669d913c8ebbe1eb1e3bf400
Sam M. Ward <smward1@hotmail.com>
2024-07-07 15:05:20 +0100

    Cleaned up script

--- analysis/perform_hyperprior_simulations.py
+++ analysis/perform_hyperprior_simulations.py
@@ -1,21 +1,21 @@
 import sys
 
-rootpath = './'
+rootpath = '../'
 sys.path.append(rootpath+'model_files/')
 from sigmaRel import *
 
 beta_prior_lines = [r"$\sigma_{0} \sim U(0,1)$", r"$\rho \sim \textrm{Arcsine}(0,1)$"]
-alt_prior_lines  = [r"$\sigma_{\rm{Rel}} \sim U(0,1)$", r"$\sigma_{\rm{Common}} \sim U(0,1)$"]
 priorA_lines     = [r"$\sigma_{0} \sim U(0,1)$", r"$\sigma_{\rm{Rel}} \sim U(0,\sigma_0)$"]
 priorB_lines     = [r"$\sigma_{0} \sim U(0,1)$", r"$\sigma_{\rm{Common}} \sim U(0,\sigma_0)$"]
 priorC_lines     = [r"$\sigma_{0} \sim U(0,1)$", r"$\rho \sim U(0,1)$"]
+#alt_prior_lines  = [r"$\sigma_{\rm{Rel}} \sim U(0,1)$", r"$\sigma_{\rm{Common}} \sim U(0,1)$"]
 
 from simulate_distances import *
 
 Ng = 100 ; Sg = 2 ; RS=10
-#for rs in np.arange(RS):
 tick_cross = dict(zip([True,False],['\\xmark','\\cmark']))
 Summary_Strs = []
+#for rs in np.arange(RS):
 for rs in [9]:
     for sigR,RHO in zip([0,0.1/((2)**0.5),0.1],[1,0.5,0]):
         simulator = SiblingsDistanceSimulator(Ng=Ng,Sg=Sg,external_distances=True,sigmaRel=sigR,zcmberr=1e-5,random=42+rs)
@@ -24,7 +24,7 @@ for rs in [9]:
         dfmus['zhelio_errs'] = dfmus['zcmb_errs']
 
         samplename = f'Ng{Ng}_Sg{Sg}_Rs{rs}_Truesigma0{simulator.sigma0}_TruesigmaRel{round(simulator.sigmaRel,3)}'
-        multigal = multi_galaxy_siblings(dfmus,samplename=samplename,sigma0=1.0,eta_sigmaRel_input=None,sigmapec=250,use_external_distances=False)
+        multigal = multi_galaxy_siblings(dfmus,samplename=samplename,sigma0=1.0,eta_sigmaRel_input=None,sigmapec=250,use_external_distances=False,rootpath=rootpath)
         multigal.n_warmup = 2000 ; multigal.n_sampling = 10000
 
         #Multi-model overlay
@@ -37,13 +37,9 @@ for rs in [9]:
 
         counter = 0 ; print (savekey);
         recovery_str= tick_cross[RHO in []]
-        multigal.sigmaRel_sampler(alt_prior=False,sigma0=sigma0,sigmapec=250,use_external_distances=True,zcosmo=zcosmo,overwrite=overwrite)
+        multigal.sigmaRel_sampler(sigma0=sigma0,sigmapec=250,use_external_distances=True,zcosmo=zcosmo,overwrite=overwrite)
         summary,postplot = multigal.plot_posterior_samples_1D(fig_ax=[None,    counter,Np,[0,-2+lind],''],returner=True); Summary_Strs.append(beta_prior_lines[-1]+' & \multirow{'+str(Np)+'}{*}{'+r'$\rho=%s$'%RHO+'} & '+' & '.join(list(summary.values())) + f'& {recovery_str} \\\\ ')
         counter += 1; print (savekey)
-
-        #multigal.sigmaRel_sampler(alt_prior=True,sigma0=sigma0,sigmapec=250,use_external_distances=True,zcosmo=zcosmo,overwrite=overwrite)
-        #postplot = multigal.plot_posterior_samples_1D(fig_ax=[postplot,counter,Np,0,'',lind])
-        #counter += 1; print (savekey)
 
         recovery_str=tick_cross[RHO in [0]]
         multigal.sigmaRel_sampler(alt_prior='A',sigma0=sigma0,sigmapec=250,use_external_distances=True,zcosmo=zcosmo,overwrite=overwrite)
